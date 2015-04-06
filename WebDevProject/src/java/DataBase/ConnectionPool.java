53f26509ede5971bb06ceff8a638bd2cb925e069
Justin Scott <Jjonscott@comcast.net>
2015-04-05 20:43:17 -0400

    login validation

---
+++ WebDevProject/src/java/DataBase/ConnectionPool.java
@@ -0,0 +1,50 @@
+package DataBase;
+
+
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class ConnectionPool {
+
+    private static ConnectionPool pool = null;
+    private static DataSource dataSource = null;
+
+    private ConnectionPool() {
+        
+        try {
+            InitialContext ic = new InitialContext();
+            dataSource = (DataSource) ic.lookup("jdbc/IntegratedWeb");
+        } catch (NamingException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static synchronized ConnectionPool getInstance() {
+        if (pool == null) {
+            pool = new ConnectionPool();
+        }
+        return pool;
+    }
+
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public void freeConnection(Connection c) {
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
--- WebDevProject/src/java/DataBase/UserDB.java
+++ WebDevProject/src/java/DataBase/UserDB.java
@@ -5,10 +5,69 @@
  */
 package DataBase;
 
+import Beans.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Justin
  */
 public class UserDB {
+    
+    public static User validateLogin(String username, String hash) {
+        
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String query = "SELECT * FROM USERS " +
+                       "WHERE PASSWORD = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setString(1, hash);
+            rs = ps.executeQuery();
+            User user = null;
+            if (rs.next() && rs.getString(username.toLowerCase()).equals(username.toLowerCase())) {
+                String date = rs.getString("USER_CREATION_DATE");
+                DateFormat format = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH);
+                user.setUser_creation_date(format.parse(date));
+
+                user = new User();
+                user.setUser_ID(Integer.parseInt(rs.getString("USER_ID")));
+                user.setStu_ID(Integer.parseInt(rs.getString("STU_ID")));
+                user.setFaculty_ID(Integer.parseInt(rs.getString("FACULTY_ID")));
+                user.setUser_address(rs.getString("USER_ADDRESS"));
+                user.setUser_city(rs.getString("USER_CITY"));
+                user.setUser_state(rs.getString("USER_STATE"));
+                user.setUser_zip(Integer.parseInt(rs.getString("USER_ZIP")));
+                user.setUser_country(rs.getString("USER_COUNTRY"));
+                user.setUser_first_name(rs.getString("USER_FIRST_NAME"));
+                user.setUser_last_name(rs.getString("USER_LAST_NAME"));
+            }
+            return user;
+        } catch (SQLException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        } catch (ParseException e) {
+            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, e);
+            return null;
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+        }
+        
+    }
     
 }
