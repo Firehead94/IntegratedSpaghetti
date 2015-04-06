327d32b8c688d00ba3d42a7f5bc38ca8d5fa7dff
Justin Scott <Jjonscott@comcast.net>
2015-04-05 20:16:19 -0400

    basic login check and md5 package

--- WebDevProject/src/java/Hash/MD5.java
+++ WebDevProject/src/java/Hash/MD5.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.hash;
+package hash;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
