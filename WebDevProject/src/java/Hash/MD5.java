2a40b86403bfe25a200c5da4a0578cee263ff92f
scensorECHO <scensorecho@github.com>
2015-04-06 19:31:04 -0400

    ATTN: may need to rename package for MD5.java

--- WebDevProject/src/java/Hash/MD5.java
+++ WebDevProject/src/java/Hash/MD5.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hash;
+package hash; //this package name may need to be modified to fit the correct package location in the project
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
