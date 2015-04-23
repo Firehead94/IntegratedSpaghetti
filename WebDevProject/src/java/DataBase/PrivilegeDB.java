99e03af3d173f9fe29cabe5e75323883f5c371e0
Justin Scott <Jjonscott@comcast.net>
2015-04-22 21:35:51 -0400

    privilege and 404

--- WebDevProject/src/java/DataBase/PrivilegeDB.java
+++ WebDevProject/src/java/DataBase/PrivilegeDB.java
@@ -20,6 +20,28 @@ import java.util.logging.Logger;
  */
 public class PrivilegeDB {
     
+    public static void newStudent(int id, int program) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String query = "INSERT INTO STUDENT (USER_ID, PROGRAM_ID) " +
+                "VALUES (?,?);";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            ps.setInt(2, program);
+            ps.executeQuery();
+        } catch (SQLException e) {
+            Logger.getLogger(PrivilegeDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+            DBUtil.closeResultSet(rs);
+        }
+    }
+    
     public static boolean isFacultyByUser(User user) {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
@@ -70,7 +92,57 @@ public class PrivilegeDB {
             DBUtil.closeResultSet(rs);
         }
         return false;        
-    }
+    }    
     
+    public static int getStudentIDByUserID(int id) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String query = "SELECT * FROM STUDENT " +
+                "WHERE USER_ID = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("STUDENT_ID");
+            }
+            return 0;
+        } catch (SQLException e) {
+            Logger.getLogger(PrivilegeDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+            DBUtil.closeResultSet(rs);
+        }
+        return 0;        
+    }   
     
+    public static int getFacultyByUserID(int id) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String query = "SELECT * FROM FACULTY " +
+                "WHERE USER_ID = ?";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt("FACULTY_ID");
+            }
+            return 0;
+        } catch (SQLException e) {
+            Logger.getLogger(PrivilegeDB.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            pool.freeConnection(connection);
+            DBUtil.closeResultSet(rs);
+        }
+        return 0;        
+    }   
 }
