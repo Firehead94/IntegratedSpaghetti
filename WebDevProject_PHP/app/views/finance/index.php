9bfb818127be7c22b6ffa0c612b7b353fa7a18bb
Firehead94 <Jjonscott@comcast.net>
2015-04-26 13:43:48 -0400

    php work

--- WebDevProject_PHP/app/views/finance/index.php
+++ WebDevProject_PHP/app/views/finance/index.php
@@ -1,4 +1,3 @@
-
 <form action="finance/login" method="post" style="display: inline-block; text-align: center; transform: translate(10%, 50%); zoom: 2">
     <input type="textbox" size="20" name="username" placeholder="username" required /><br /><br />
     <input type="password" size="20" name="password" placeholder="password" required /><br /><br />
--- WebDevProject_PHP/app/views/finance/payment.php
+++ WebDevProject_PHP/app/views/finance/payment.php
@@ -1,1 +1,34 @@
-payment
+<form id="pay" action="payment" method="post" >
+    <script>
+    function selected() {
+        var url = window.location.href;
+        if (document.getElementById('select').value === 'new') {            
+            if (url.indexOf('?') > -1){
+                url = url.substring(0,url.indexOf('?')) + '?option=' + document.getElementById('select').value
+             }else{
+                 url += '?option=' + document.getElementById('select').value
+             }
+           
+            window.location.href = url;
+        } else {
+            window.location.href = url.substring(0,url.indexOf('?'));
+        }
+    }    
+    
+    </script>
+        
+
+    <select name="paymentoptions" form="pay" onchange="selected()" id="select">
+        <option value="">Select...</option>
+        <?php foreach($_SESSION['financialinfo'] as $key => $val )  {?>
+        <option value="<?php $val['CREDITCARD_NUM'] ?>">CCN:<?php echo $val['CREDITCARD_NUM'] ?>
+            &nbsp;&nbsp;EXP:<?php echo $val['EXP_DATE'] ?></option>
+        <?php } ?>
+        <option value="new">New Payment Method</option>
+    </select>
+    
+    <?php if(isset($_REQUEST['option']) && $_REQUEST['option'] = 'new') { ?>
+        place input form here for getting financial info for new card
+    <?php }?>
+    
+</form>
